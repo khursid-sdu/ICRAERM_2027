2a395cd1d4babf86e23e67fa19e4a005c643d35d
khursid-sdu <khursid@gift.edu.in>
2026-09-25 17:32:03 +0530

    Add files via upload

---
+++ js/countdown-custom.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function() {
+    $(function () {
+        $('#defaultCountdown').countdown({until: new Date(2027, 3-1, 18, 9, 0, 0)}); // 18 March 2027, 09:00 AM
+    });
+});		
+
